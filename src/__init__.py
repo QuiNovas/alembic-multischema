4648e990144687306a5f05d51ca80549a21b019a
qmoon <mmoon@quinovas.com>
2019-10-16 14:03:00 -0500

    Add function to return even 'system' schemas return empty list on get schemas errors

--- src/__init__.py
+++ src/__init__.py
@@ -1,26 +1,33 @@
 from alembic import op
 from sqlalchemy.sql import text
 
-def getAllSchemas():
-  try:
-    conn = op.get_bind()
-    schemas = conn.execute(text("SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('information_schema', 'pg_catalog')")).fetchall()
-    return [ str(x[0]) for x in schemas ]
-  except:
-    return []
+def getAllNonSystemSchemas():
+    try:
+      conn = op.get_bind()
+      schemas = conn.execute(text("SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('information_schema', 'pg_catalog')")).fetchall()
+      return [ str(x[0]) for x in schemas ]
+    except:
+      return []
 
-def perSchema(schemas=getAllSchemas()):
+def getAllSchemas():
+    try:
+      conn = op.get_bind()
+      schemas = conn.execute(text("SELECT schema_name FROM information_schema.schemata")).fetchall()
+      return [ str(x[0]) for x in schemas ]
+    except:
+      return []
+
+def perSchema(schemas=getAllNonSystemSchemas()):
   def externalWrapper(func):
     def wrapper(*args, **kwargs):
       for s in schemas:
-        print(schemas)
         print("Executing on schema {}".format(s))
         try:
           op.execute(text("SET search_path TO {}".format(s)))
           func(*args, **kwargs)
           op.execute(text("SET search_path TO default"))
         except Exception as e:
-          print(e)
-          raise SystemExit
+          op.execute(text("SET search_path TO default"))
+          raise e
     return wrapper
   return externalWrapper
