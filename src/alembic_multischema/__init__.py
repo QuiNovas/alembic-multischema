569447e27db8e13eb1a8b38c0c69a9035211c00c
qmoon <mmoon@quinovas.com>
2019-10-16 16:40:28 -0500

    Add exclude option

--- src/alembic_multischema/__init__.py
+++ src/alembic_multischema/__init__.py
@@ -1,26 +1,26 @@
 from alembic import op
 from sqlalchemy.sql import text
 
-def getAllNonSystemSchemas():
+def getNonSystemSchemas(exclude=[]):
     try:
       conn = op.get_bind()
       schemas = conn.execute(text("SELECT schema_name FROM information_schema.schemata WHERE schema_name NOT IN ('information_schema', 'pg_catalog')")).fetchall()
-      return [ str(x[0]) for x in schemas ]
+      return [ str(x[0]) for x in schemas if x not in exclude]
     except:
       return []
 
-def getAllSchemas():
+def getSchemas(exclude=[]):
     try:
       conn = op.get_bind()
       schemas = conn.execute(text("SELECT schema_name FROM information_schema.schemata")).fetchall()
-      return [ str(x[0]) for x in schemas ]
+      return [ str(x[0]) for x in schemas if x not in exclude ]
     except:
       return []
 
-def perSchema(schemas=getAllNonSystemSchemas()):
+def perSchema(schemas=getNonSystemSchemas(), exclude=[]):
   def externalWrapper(func):
     def wrapper(*args, **kwargs):
-      for s in schemas:
+      for s in schemas if s not in exclude:
         print("Executing on schema {}".format(s))
         try:
           op.execute(text("SET search_path TO {}".format(s)))
